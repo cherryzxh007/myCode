108f99377c07d2415baea95e7e93390004ec1f17
cherryzxh007 <cherryzxh007@gmail.com>
2014-05-15 16:57:07 +0800

    git branch backcup

--- helloworld/src/helloworld/HelloGit.java
+++ helloworld/src/helloworld/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
  public void test(){
 	 System.out.println("helloworld");
 	 System.out.println("ok");
-	 System.out.println("so gar");
+	 System.out.println("new branch");
+	 
  }
 }
